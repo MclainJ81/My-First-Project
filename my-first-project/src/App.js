7632949ed2963a88bd5e3ab7a54f64855df6dc15
Jeff Mclain <jeffrey.mclain81@gmail.com>
2019-07-28 16:10:50 -0500

    added new background

--- my-first-project/src/App.js
+++ my-first-project/src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="app-text
+      ">
           Today is the day
         </p>
         <a
